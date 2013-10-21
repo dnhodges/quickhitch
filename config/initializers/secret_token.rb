04674bbde4c2ab9742b4b21df96bfce3a032355d
Nic Hodges <dnhodges@gmail.com>
2013-10-21 13:43:49 -0400

    Initialize repository.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quickhitch::Application.config.secret_key_base = '6d85429ac868d438cd8946f5c5f6195cc205192b08a6a81d459b8bbd085963bff2fc8b0a990a2f32847ff572a896ada9aa02e736d8c29d97064731743e3f3ee1'
